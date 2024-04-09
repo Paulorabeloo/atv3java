d3d43ddc2192ed48e3201bab6e6bf92dd06df3ac
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-04-09 13:53:51 -0300

    aula1ex4

---
+++ Aula1ex4.java
@@ -0,0 +1,15 @@
+package com.mycompany.aula1ex4;
+
+public class Aula1ex4 
+{
+    public static void main(String[] args) 
+    {
+    double raio = 700; 
+    double pi = 3.1416;
+    double area = 0;
+    
+    area = (raio*raio) * pi;
+    
+    System.out.println("Resultado: " +area);
+    }
+}
