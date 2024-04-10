96caf5264e255704b5d0886e853479c94732c2b3
Paulo Rabelo <pauloeduardorabelomiranda@gmail.com>
2024-04-10 10:19:36 -0300

    Aula1ex4.java

--- Aula1ex4.java
+++ Aula1ex4.java
@@ -4,12 +4,12 @@ public class Aula1ex4
 {
     public static void main(String[] args) 
     {
-    double raio = 700; 
-    double pi = 3.1416;
-    double area = 0;
+        double raio = 700; 
+        double pi = 3.1416;
+        double area = 0;
     
-    area = (raio*raio) * pi;
+        area = (raio*raio) * pi;
     
-    System.out.println("Resultado: " +area);
+        System.out.println("Resultado: " +area);
     }
 }
